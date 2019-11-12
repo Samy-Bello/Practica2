40e98a4b629200be2312bdf1f78e83844cce3805
OscarOcb <oscar.ocb7@gmail.com>
2019-11-12 15:45:48 +0100

    Initial commit

---
+++ Practica2.sh
@@ -0,0 +1,11 @@
+echo "Practica 2"
+
+
+
+git init .
+git remote add origin https://github.com/FonamentsEnginyeria/practica-2-oscar-samantha.git
+git add *
+git config --global user.email "oscar.ocb7@gmail.com"
+git config --global user.name "OscarOcb"
+git commit -m "Initial commit"
+git push -u origin master
